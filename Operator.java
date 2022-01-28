c5d2e68c8721020880e09e76bf310f16445b3d2f
sukhmeet singh <sukhmeets111@gmail.com>
2022-01-28 14:45:08 +0530

    made some changes

--- Operator.java
+++ Operator.java
@@ -4,7 +4,8 @@ public class Operator {
         int a=0,b=9,c;
         c=a+b;
         System.out.println(" The sum of two numbers is "+c);
-        System.out.print(a<c);
+        System.out.println(a<c);
+        System.out.print(4.5/4);
         //arithmetic operators = " + , - , * , / , % "
         //logical =" && ||"
         //comparison =" < , > , == , >= , <= , != "
