d612baeb29b3c43969456d4052c49dc0b0b7bc17
sukhmeet singh <sukhmeets111@gmail.com>
2022-01-28 14:39:16 +0530

    operators

---
+++ Operator.java
@@ -0,0 +1,15 @@
+// import java.util.*;
+public class Operator {
+    public static void main(String[] args) {
+        int a=0,b=9,c;
+        c=a+b;
+        System.out.println(" The sum of two numbers is "+c);
+        System.out.print(a<c);
+        //arithmetic operators = " + , - , * , / , % "
+        //logical =" && ||"
+        //comparison =" < , > , == , >= , <= , != "
+        //assignment = " = , += , -= "
+        //increment =" ++ , -- "
+        //bitwise = " & , | "
+    }
+}
